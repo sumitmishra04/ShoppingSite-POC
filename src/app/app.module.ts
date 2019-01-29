8380ee77afe95c4c4319291dc914c9af52cfb668
sumit <mishrasumit042@gmail.com>
2019-01-29 16:34:31 +0530

    initial checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BreadCrumbComponent } from './bread-crumb/bread-crumb.component';
+import { ShoppingModule } from './shopping/shopping.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BreadCrumbComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShoppingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
